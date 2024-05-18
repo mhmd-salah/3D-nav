538f76922cff909ac7247f3450c4a7bf65f66b8f
mhmd <ms.mhmdsalah@gmail.com>
2024-05-18 19:05:52 +0300

    the end

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,30 @@
 "use strict";
+// hello mohamed salah
+let container = document.querySelector(".page-container");
+let pages = document.querySelectorAll(".page");
+let toggleBtn = document.querySelector(".toggle-btn");
+let ul = document.querySelector(".nav-list");
+let overlay = document.querySelector(".overlay");
+let links = document.querySelectorAll(".link");
+let pageIndex = 0;
+toggleBtn === null || toggleBtn === void 0 ? void 0 : toggleBtn.addEventListener("click", (e) => {
+    toggleBtn.classList.toggle("active");
+    container === null || container === void 0 ? void 0 : container.classList.toggle("active");
+    ul === null || ul === void 0 ? void 0 : ul.classList.toggle("show");
+});
+links.forEach((link, i) => {
+    link.addEventListener("click", () => {
+        nextPage(i);
+    });
+});
+function nextPage(index) {
+    overlay.style.animation = "slid 1s linear 1";
+    setTimeout(() => {
+        pages[pageIndex].classList.remove("active");
+        pages[index].classList.add("active");
+        pageIndex = index;
+    }, 500);
+    setTimeout(() => {
+        overlay.style.animation = "";
+    }, 1000);
+}
